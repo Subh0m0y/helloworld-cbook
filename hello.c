e0a14a4d81058d25453b9ec72412fc9f12e2027f
Subhomoy Haldar <subhomoyhaldar@gmail.com>
2017-10-25 21:58:24 +0530

    This is the first commit. Contains the hello.c file.

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * This is the first program that all programmers write.
+ * It's not compulsory, it just a tradition.
+ */
+int main()
+{
+	printf("Hello, world!\n");
+	return EXIT_SUCCESS;
+}
